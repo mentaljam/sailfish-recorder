584a287fded3bd4e37aaa54d4fe094058b2f57e6
Petr Tsymbarovich <petr@tsymbarovich.ru>
2017-06-16 02:10:16 +0300

    Update Russian translation

--- translations/harbour-recorder-ru.ts
+++ translations/harbour-recorder-ru.ts
@@ -4,87 +4,142 @@
 <context>
     <name>CoverPage</name>
     <message>
-        <location filename="../qml/cover/CoverPage.qml" line="29"/>
+        <location filename="../qml/cover/CoverPage.qml" line="36"/>
         <source>Record</source>
         <translation>Записать</translation>
     </message>
     <message>
-        <location filename="../qml/cover/CoverPage.qml" line="31"/>
-        <source>Stop</source>
-        <translation>Остановить</translation>
+        <location filename="../qml/cover/CoverPage.qml" line="38"/>
+        <source>Recording...</source>
+        <translation>Запись...</translation>
     </message>
     <message>
-        <location filename="../qml/cover/CoverPage.qml" line="33"/>
-        <source>Resume</source>
-        <translation>Продолжить</translation>
+        <location filename="../qml/cover/CoverPage.qml" line="40"/>
+        <source>Paused</source>
+        <translation>Пауза</translation>
     </message>
 </context>
 <context>
     <name>FirstPage</name>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="78"/>
+        <location filename="../qml/pages/FirstPage.qml" line="64"/>
         <source>Recordings</source>
         <translation>Записи</translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="153"/>
+        <location filename="../qml/pages/FirstPage.qml" line="76"/>
         <source>Settings</source>
         <translation>Настройки</translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="157"/>
-        <source>Refresh</source>
-        <translation>Обновить</translation>
+        <location filename="../qml/pages/FirstPage.qml" line="83"/>
+        <source>Your recordings will be shown here</source>
+        <translation>Ваши записи появятся здесь</translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="159"/>
-        <source>Refreshing</source>
-        <translation>Обновление</translation>
+        <location filename="../qml/pages/FirstPage.qml" line="167"/>
+        <source>Recording...</source>
+        <translation>Запись...</translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="180"/>
-        <source>Deleting</source>
-        <translation>Удаление</translation>
+        <location filename="../qml/pages/FirstPage.qml" line="177"/>
+        <source>Paused</source>
+        <translation>Пауза</translation>
     </message>
+</context>
+<context>
+    <name>MigrationPage</name>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="200"/>
-        <source>Delete</source>
-        <translation>Удалить</translation>
-    </message>
-    <message>
-        <location filename="../qml/pages/FirstPage.qml" line="204"/>
-        <source>Rename</source>
-        <translation>Переименовать</translation>
-    </message>
-    <message>
-        <location filename="../qml/pages/FirstPage.qml" line="227"/>
-        <source>Rename File</source>
-        <translation>Переименовать файл</translation>
-    </message>
-    <message>
-        <location filename="../qml/pages/FirstPage.qml" line="232"/>
-        <source>New filename</source>
-        <translation>Новое имя файла</translation>
-    </message>
-    <message>
-        <location filename="../qml/pages/FirstPage.qml" line="310"/>
-        <source>Migration</source>
-        <translation>Обновление</translation>
-    </message>
-    <message>
-        <location filename="../qml/pages/FirstPage.qml" line="314"/>
-        <source>The default folder has changed from &quot;%0&quot; to &quot;%1&quot;. Do you want to move existing recordings to the new folder? If you cancel the old directory will be kept.</source>
-        <translation>Стандартный каталог записи изменен с &quot;%0&quot; на &quot;%1&quot;. Перенести существующие записи в новое местоположение? При отказе старый каталог будет сохранен.</translation>
-    </message>
-    <message>
-        <location filename="../qml/pages/FirstPage.qml" line="325"/>
+        <location filename="../qml/pages/MigrationPage.qml" line="7"/>
         <source>Migration failed, old recordings should still be in the old folder.</source>
         <translation>Обновление не завершено. Старые записи остались в старом каталоге.</translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="327"/>
-        <source>Migration.</source>
-        <translation>Обновление.</translation>
+        <location filename="../qml/pages/MigrationPage.qml" line="9"/>
+        <source>Migration finished</source>
+        <translation>Обновление закончено</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MigrationPage.qml" line="17"/>
+        <source>Migration</source>
+        <translation>Обновление</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MigrationPage.qml" line="27"/>
+        <source>The default folder has changed from &quot;%0&quot; to &quot;%1&quot;. Do you want to move existing recordings to the new folder? If you cancel the old directory will be kept.</source>
+        <translation>Стандартный каталог записи изменен с &quot;%0&quot; на &quot;%1&quot;. Перенести существующие записи в новое местоположение? При отказе старый каталог будет сохранен.</translation>
+    </message>
+</context>
+<context>
+    <name>Recorder</name>
+    <message>
+        <location filename="../src/recorder.cpp" line="108"/>
+        <source>recording</source>
+        <translation>запись</translation>
+    </message>
+</context>
+<context>
+    <name>RecordingDelegate</name>
+    <message>
+        <location filename="../qml/components/RecordingDelegate.qml" line="8"/>
+        <source>Rename</source>
+        <translation>Переименовать</translation>
+    </message>
+    <message>
+        <location filename="../qml/components/RecordingDelegate.qml" line="15"/>
+        <source>Delete</source>
+        <translation>Удалить</translation>
+    </message>
+    <message>
+        <location filename="../qml/components/RecordingDelegate.qml" line="17"/>
+        <source>Deleting</source>
+        <translation>Удаление</translation>
+    </message>
+</context>
+<context>
+    <name>RecordingsModel</name>
+    <message>
+        <location filename="../src/recordingsmodel.cpp" line="122"/>
+        <source>Older</source>
+        <translation>Старше</translation>
+    </message>
+    <message>
+        <location filename="../src/recordingsmodel.cpp" line="120"/>
+        <source>Last 6 months</source>
+        <translation>Последние полгода</translation>
+    </message>
+    <message>
+        <location filename="../src/recordingsmodel.cpp" line="116"/>
+        <source>Last month</source>
+        <translation>Последний месяц</translation>
+    </message>
+    <message>
+        <location filename="../src/recordingsmodel.cpp" line="112"/>
+        <source>This week</source>
+        <translation>Эта неделя</translation>
+    </message>
+    <message>
+        <location filename="../src/recordingsmodel.cpp" line="108"/>
+        <source>Yesterday</source>
+        <translation>Вчера</translation>
+    </message>
+    <message>
+        <location filename="../src/recordingsmodel.cpp" line="104"/>
+        <source>Today</source>
+        <translation>Сегодня</translation>
+    </message>
+</context>
+<context>
+    <name>RenameDialog</name>
+    <message>
+        <location filename="../qml/pages/RenameDialog.qml" line="21"/>
+        <source>Rename File</source>
+        <translation>Переименовать файл</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/RenameDialog.qml" line="27"/>
+        <source>New filename</source>
+        <translation>Новое имя файла</translation>
     </message>
 </context>
 <context>
@@ -100,34 +155,42 @@
         <translation>Каталог записей</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Settings.qml" line="23"/>
+        <location filename="../qml/pages/Settings.qml" line="21"/>
         <source>Codec</source>
         <translation>Кодек</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Settings.qml" line="46"/>
+        <location filename="../qml/pages/Settings.qml" line="42"/>
         <source>Vorbis is a good choice for music. Speex is a good choice for speech. PCM and FLAC are both a lossless format.</source>
         <translation>Vorbis хорошо подходит для записи музыки, Speex – для речи. PCM и FLAC позволяют вести запись без потери качества.</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Settings.qml" line="66"/>
+        <location filename="../qml/pages/Settings.qml" line="62"/>
         <source>Sample Rate</source>
         <translation>Частота дискретизации</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Settings.qml" line="75"/>
+        <location filename="../qml/pages/Settings.qml" line="69"/>
         <source>auto</source>
         <translation>Авто</translation>
     </message>
     <message>
+        <location filename="../qml/pages/Settings.qml" line="73"/>
+        <location filename="../qml/pages/Settings.qml" line="76"/>
         <location filename="../qml/pages/Settings.qml" line="79"/>
         <location filename="../qml/pages/Settings.qml" line="82"/>
         <location filename="../qml/pages/Settings.qml" line="85"/>
         <location filename="../qml/pages/Settings.qml" line="88"/>
-        <location filename="../qml/pages/Settings.qml" line="91"/>
-        <location filename="../qml/pages/Settings.qml" line="94"/>
         <source>%0kHz</source>
         <translation>%0 кГц</translation>
+    </message>
+</context>
+<context>
+    <name>harbour-recorder</name>
+    <message>
+        <location filename="../qml/harbour-recorder.qml" line="53"/>
+        <source>No space left on the device</source>
+        <translation>Недостаточно свободного места на устройстве</translation>
     </message>
 </context>
 </TS>
